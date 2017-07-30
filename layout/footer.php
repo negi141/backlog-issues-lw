723d203567dddbbba9669bd2de36e60e1e258cd9
negi <takuto.negishi@gmail.com>
2017-07-30 18:19:41 +0900

    フッターにリポジトリへのリンク追加

--- layout/footer.php
+++ layout/footer.php
@@ -1,4 +1,7 @@
     <hr>
-    <footer>Created by Negishi</footer>
+    <footer>
+        Created by Negishi. 
+        <a href="https://github.com/negi141/backlog-issues-lw" target="_blank">Repository</a>
+    </footer>
     </body>
 </html>
