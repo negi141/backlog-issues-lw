817430cdc75fb0f6ab58fc9ac251a8de76f23d0e
negi <takuto.negishi@gmail.com>
2017-08-20 16:47:06 +0900

    sender

--- sender.php
+++ sender.php
@@ -7,44 +7,14 @@ require_once "lib/backlog.php";
 require __DIR__ . '/vendor/autoload.php'; // path to vendor/
 
 main();
+
 function main(){
     $span = (object)["start" => strtotime("-10 day"), "end" => strtotime("+20 day")];
     $issues = getIssues(getenv("BACKLOG_API_KEY"), $span);
     $content = formatIssues($issues);
     sendMail($content);
 }
-function formatIssues($issues) {
-        
-    $html = "";
-    // Html生成
-    foreach ($issues as $lwverKey => $lwver) {
-        $html .= Html::tag('h2', $lwverKey);
-        foreach ($lwver as $daysKey => $days) {
-            $date = new DateTime($daysKey);
-            $date->add(new DateInterval('P1D'));
-            $now = new DateTime();
-            $interval = $date->diff($now);
-            if ($now < $date) {
-                $dateMessage = $interval->d . "日後";
-            } else {
-                $dateMessage = $interval->d . "日前";
-            }
-            $html .= Html::tag('h3', substr($daysKey, 0, 10) . "　" . $dateMessage);
-            foreach ($days as $issue) {
-                $url = sprintf(BacklogApi::WEB_URL, 'esk-sys', $issue['issueKey']);
-                $summary = preg_replace("/\[.+\]/", "", $issue['summary']);
-                $summary = preg_replace("/【.+】/", "", $summary);
-                $summary = Util::leftString($summary, 30);
-                $statusColor = ["未対応" => "#ED8077", "処理中" => "#4488C5", "処理済み" => "#5EB5A6"];
-                $stagusName = Html::tag("span", $issue['status']['name'], "style='color:" . $statusColor[$issue['status']['name']] . ";'");
-                $summary = Html::tag('a', $summary, "href='$url' target='_blank'");
-                $html .= Html::tag('li',
-                        "{$summary} 　{$issue['assignee']['name']} 　{$stagusName}") . PHP_EOL;
-            }
-        }
-    }
-    return $html;
-}
+
 function getIssues($apiKey, $span)
 {
     $backlogApi = new BacklogApi($apiKey, 'esk-sys');
@@ -79,33 +49,51 @@ function getIssues($apiKey, $span)
     return $formattedIssues;
 }
 
-function sendMail($content) {
-    $request_body = json_decode('{
-    "personalizations": [
-        {
-        "to": [
-            {
-            "email": "'.getenv("MAIL_TO").'"
+function formatIssues($issues) {
+        
+    $html = "";
+    // Html生成
+    foreach ($issues as $lwverKey => $lwver) {
+        $html .= Html::tag('h2', $lwverKey);
+        foreach ($lwver as $daysKey => $days) {
+            $date = new DateTime($daysKey);
+            $date->add(new DateInterval('P1D'));
+            $now = new DateTime();
+            $interval = $date->diff($now);
+            if ($now < $date) {
+                $dateMessage = $interval->d . "日後";
+            } else {
+                $dateMessage = $interval->d . "日前";
             }
-        ],
-        "subject": "backlog issues for lw"
+            $html .= Html::tag('h3', substr($daysKey, 0, 10) . "　" . $dateMessage);
+            foreach ($days as $issue) {
+                $url = sprintf(BacklogApi::WEB_URL, 'esk-sys', $issue['issueKey']);
+                $summary = preg_replace("/\[.+\]/", "", $issue['summary']);
+                $summary = preg_replace("/【.+】/", "", $summary);
+                $summary = Util::leftString($summary, 30);
+                $statusColor = ["未対応" => "#ED8077", "処理中" => "#4488C5", "処理済み" => "#5EB5A6"];
+                $stagusName = Html::tag("span", $issue['status']['name'], "style='color:" . $statusColor[$issue['status']['name']] . ";'");
+                $summary = Html::tag('a', $summary, "href='$url' target='_blank'");
+                $html .= Html::tag('li',
+                        "{$summary} 　{$issue['assignee']['name']} 　{$stagusName}") . PHP_EOL;
+            }
         }
-    ],
-    "from": {
-        "email": "'.getenv("MAIL_FROM").'"
-    },
-    "content": [
-        {
-        "type": "text/plain",
-        "value": "'.$content.'"
-        }
-    ]
-    }');
+    }
+    return $html;
+}
+
+function sendMail($mailBody) {
+    $from = new SendGrid\Email(null, getenv("MAIL_FROM"));
+    $subject = "backlog issues for lw";
+    $to = new SendGrid\Email(null, getenv("MAIL_TO"));
+    $content = new SendGrid\Content("text/plain", $mailBody);
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
     $apiKey = getenv('SENDGRID_API_KEY');
     $sg = new \SendGrid($apiKey);
 
-    $response = $sg->client->mail()->send()->post($request_body);
+    $response = $sg->client->mail()->send()->post($mail);
+
     echo $response->statusCode();
     echo $response->body();
     echo $response->headers();
