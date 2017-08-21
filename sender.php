4079fbc147a3d19534e09ecf76cf43c712b42dba
negi <takuto.negishi@gmail.com>
2017-08-21 21:11:09 +0900

    style

--- sender.php
+++ sender.php
@@ -21,7 +21,7 @@ function formatIssues($issues) {
     $html = "";
     // Html生成
     foreach ($issues as $lwverKey => $lwver) {
-        $html .= Html::tag('h2', $lwverKey);
+        $html .= Html::tag('h2', $lwverKey, "border-bottom: 1px solid #cdf;");
         foreach ($lwver as $daysKey => $days) {
             $date = new DateTime($daysKey);
             $date->add(new DateInterval('P1D'));
@@ -52,7 +52,7 @@ $header = <<<HTML
 <head>
     <meta charset="utf-8">
 </head>
-<body>
+<body style="font-family: helvetica, arial, 'lucida grande', 'hiragino kaku gothic pro', meiryo, 'ms pgothic', sans-serif;">
 このメールは<a href="https://backlog-issues-lw.herokuapp.com/" target="_blank">backlog Checker for LW</a>から
 自動で送られています。(一日一回)
 HTML;
